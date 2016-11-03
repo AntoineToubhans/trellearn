2e77ba865d4d9b813c44c4901992f2f215122934
Antoine Toubhans <antoine@toubhans.org>
2016-11-03 15:29:57 +0100

    Add trellearn order button in Trellearn plugin

--- plugin/trellearn.js
+++ plugin/trellearn.js
@@ -11,22 +11,79 @@ $(document)
 
   syncButton = document.createElement('button');
   syncButton.classList.add('trellearn-button');
+  syncButton.classList.add('trellearn-sync');
   syncButton.innerText = 'Sync Trellearn';
   syncButton.onclick = sync;
 
+  orderButton = document.createElement('button');
+  orderButton.classList.add('trellearn-button');
+  orderButton.classList.add('trellearn-order');
+  orderButton.innerText = 'Trellearn order';
+  orderButton.onclick = order;
+
+  document.body.appendChild(orderButton);
   document.body.appendChild(syncButton);
 
-  fetchBoard()
+  fetchBoard();
 });
 
-function sync() {
-  console.log('lol');
+function order(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var matches = window.location.href.match(/\/(.{8})\//);
+  Trellearn.cardShortLink = matches[1];
+  console.log('The card short Id is:', Trellearn.cardShortLink);
+  $.getJSON('https://trello.com/1/cards/'+Trellearn.cardShortLink+'?fields=all', function(card) {
+    Trellearn.card = card;
+    sendCard();
+  });
+}
+
+function sendCard(){
+  console.log(Trellearn)
+  $.ajax({
+    type: 'post',
+    url: 'http://localhost:8000/labels',
+    data: JSON.stringify(_.pick(Trellearn, ['card', 'boardId'])),
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    traditional: true,
+    success: reorderLabels,
+  });
+}
+
+function reorderLabels(orderedLabels) {
+  ul = document.querySelector('.edit-labels-pop-over');
+  if(ul) {
+    for (i in orderedLabels) {
+      span = ul.querySelector('li span[data-idlabel="'+ orderedLabels[i].id +'"]');
+      li = span.parentElement;
+      orderedLabels[i].li = li;
+    }
+    while(ul.firstChild){
+      ul.removeChild(ul.firstChild);
+    }
+    for (i in orderedLabels) {
+      ul.appendChild(orderedLabels[i].li);
+    }
+  }
+}
+
+function sync(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $.getJSON('https://trello.com/1/boards/'+Trellearn.boardId+'/cards?fields=all', function(cards) {
+    console.log('Sync: ' + cards.length + ' card(s) fetched');
+    Trellearn.cards = cards;
+    sendCards();
+  });
+}
+
+function sendCards(){
   $.ajax({
     type: 'post',
     url: 'http://localhost:8000/learn',
-    data:  JSON.stringify({
-      board: [{ coucou: 'lol' }],
-    }),
+    data: JSON.stringify(_.pick(Trellearn, ['cards', 'boardId'])),
     dataType: 'json',
     contentType: "application/json; charset=utf-8",
     traditional: true,
@@ -37,10 +94,9 @@ function sync() {
 }
 
 function fetchBoard() {
-  // tokens=all&sessions=all&credentials=all&logins=true
   $.getJSON('https://trello.com/1/members/me/boards?filter=open&fields=shortLink', function(boards) {
     var board = _.find(boards, ['shortLink', Trellearn.boardShortLink]);
     Trellearn.boardId = board.id;
-    console.log('Your board id is:', Trellearn.boardId);
+    console.log('Your board ID is:', Trellearn.boardId);
   });
 }
