bffbeb3985f81dab58b0adb8af3759825e5eca4b
Antoine Toubhans <antoine@toubhans.org>
2016-12-07 08:53:37 +0100

    Remove comments

--- main.py
+++ main.py
@@ -2,10 +2,8 @@
 
 import sys
 import src.json_importing as I
-import src.json_preprocessing_naive as P
 import src.data_training as T
 import src.data_cross_validation as V
-#import src.extract_feature as E
 import src.extract_feature_multilabel as EML
 
 if __name__ == '__main__':
@@ -18,10 +16,5 @@ if __name__ == '__main__':
 
     X, Y, cv, mlb = EML.extract(cards)
     V.validateML(X, Y)
-
-#    X, y = P.extractDataFromCards(cards)
-#    V.search(X, y)
-
-    #print(clf.predict(X[2:3]))
 
     exit(0)
